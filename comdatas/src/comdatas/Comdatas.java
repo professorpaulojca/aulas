a7bf5852cb5ffb466027123eb9730f061c502257
professorpaulojca <43889303+professorpaulojca@users.noreply.github.com>
2019-05-24 16:06:26 -0300

    comdatas

--- comdatas/src/comdatas/Comdatas.java
+++ comdatas/src/comdatas/Comdatas.java
@@ -7,8 +7,10 @@ package comdatas;
 
 import daos.MovimentoDao;
 import java.sql.Time;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import modelos.Movimentodedatas;
 
 /**
@@ -20,52 +22,58 @@ public class Comdatas {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
-        
+
         ///atribuir valores ao objeto
         Movimentodedatas objmovimentodedatas = new Movimentodedatas();
-        
+
         //pega a data e hora atual do sistema
         java.util.Date hoje = new java.util.Date();
-        
+
         //pega a hora atual do sitema
         Time hora = new Time(hoje.getTime());
-        
+
         objmovimentodedatas.setSomentedata(new java.sql.Timestamp(hoje.getTime()));
         objmovimentodedatas.setSomentehora(hora);
         objmovimentodedatas.setDataehorajuntos(hoje);
-        
+
         MovimentoDao movimentodao = new MovimentoDao();
         movimentodao.cadastrar(objmovimentodedatas);
-        
+
         ///////////////////////////////////////////////////////////////////////
-        
         ArrayList<Movimentodedatas> arr = new ArrayList<Movimentodedatas>();
         arr = movimentodao.listar(objmovimentodedatas);
-        
+
         for (Movimentodedatas obj : arr) {
-            
+
             System.out.println("Data:" + obj.getSomentedata());
             System.out.println("Hora:" + obj.getSomentehora());
             System.out.println("Data e Hora Juntos:" + obj.getDataehorajuntos());
-            
-            //diferença
+
+            //diferença com time stamp
             long diferenca = hoje.getTime() - obj.getDataehorajuntos().getTime();
-            long diferencaemminutos = diferenca / (60 * 1000); 
-            long diferencaemhoras = diferenca / (60 * 60 * 1000); 
-            
-            
+            long diferencaemminutos = diferenca / (60 * 1000);
+            long diferencaemhoras = diferenca / (60 * 60 * 1000);
+
             System.out.println("Difereça em minutos:" + diferencaemminutos);
             System.out.println("Difereça em horas:" + diferencaemhoras);
-            
-            
+            /////
+
+            //diferença com data e hora separados
+            SimpleDateFormat formatar = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date dataehora = formatar.parse(obj.getSomentedata() + " " + obj.getSomentehora());
+
+            long diferencadh = hoje.getTime() - dataehora.getTime();
+            long diferencaemminutosdh = diferencadh / (60 * 1000);
+            long diferencaemhorasdh = diferencadh / (60 * 60 * 1000);
+
+            System.out.println("Difereça em minutos data separada:" + diferencaemminutosdh);
+            System.out.println("Difereça em horas data separada:" + diferencaemhorasdh);
+
         }
-        
+
         //////////////////////////////////////////////////////////////////////
-       
-        
-        
     }
-    
+
 }
